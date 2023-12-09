29f95cb7fb1a7d37c834f61cb6d83b98613c045f
KLkapelka <153184633+KLkapelka@users.noreply.github.com>
2023-12-09 11:13:49 +0700

    Create Program.cs

---
+++ project/Program.cs
@@ -0,0 +1,24 @@
+﻿// Дан файл numsTask4.txt с целыми числами, расположенными через пробел.
+// Определите количество одинаковых рядом стоящих чисел;
+
+
+using System;
+using System.IO;
+class work_4_4
+{
+    static void Main()
+    {
+        string[] lines = File.ReadAllLines("numsTask4.txt"); 
+        int[] numbers = lines[0].Split(' ').Select(int.Parse).ToArray(); 
+        int sum = 0;
+
+        for (int i = 1; i < numbers.Length; i++)
+        {
+            if (numbers[i - 1] == numbers[i])
+            {
+                sum ++;
+            }
+        }
+        Console.WriteLine($"Одинаковых рядом стоящих чисел: {sum}");
+    }
+}  
